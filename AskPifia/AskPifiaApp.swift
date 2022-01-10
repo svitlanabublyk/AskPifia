8c59f7a7ed60a1c37e53a2e3df4f597f75d863f8
Svitlana Bublyk <lana.bublyk@gmail.com>
2022-01-10 18:26:24 +0100

    Initial Commit

---
+++ AskPifia/AskPifiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskPifiaApp.swift
+//  AskPifia
+//
+//  Created by Svitlana Bublyk on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AskPifiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

63b80df8a8e088a04d7213d74c6a50ccd61264cd
Svitlana Bublyk <lana.bublyk@gmail.com>
2022-01-13 19:43:02 +0100

    add API request

--- AskPifia/Model.swift
+++ AskPifia/Model.swift
@@ -8,3 +8,37 @@
 import Foundation
 
 
+
+struct Answer: Decodable {
+    var magic: Request
+}
+
+
+struct Request: Decodable {
+    var question: String
+    var answer: String
+    var type: String
+    
+}
+
+
+class Api{
+    func getAnswer (completion: @escaping (Answer) ->()) {
+        let apiAnswer = "https://8ball.delegator.com/magic/JSON/question_string"
+        guard let url = URL(string: apiAnswer)
+        else {return}
+        URLSession.shared.dataTask(with: url) { (data, _, _)  in
+            let answer = try! JSONDecoder().decode(Answer.self, from: data!)
+            DispatchQueue.main.async {
+                completion(answer)
+            }
+        }
+        .resume()
+    }
+}
+
+
+
+
+
+

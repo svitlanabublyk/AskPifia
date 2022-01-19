957377cf4fdf910cfc63d4ea1ab4eaff13bb2c33
Svitlana Bublyk <lana.bublyk@gmail.com>
2022-01-19 20:08:57 +0100

    work without Internet connection

---
+++ SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  AskPifia
+//
+//  Created by Svitlana Bublyk on 14/01/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
